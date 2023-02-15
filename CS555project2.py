1d7189d64bc0f35eb7bd1e139bda40aa749ea01f
18773359903 <124096068+18773359903@users.noreply.github.com>
2023-02-15 00:46:02 -0500

    Update CS555project2.py

--- CS555project2.py
+++ CS555project2.py
@@ -1,16 +1,2 @@
 supported_tags = ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", "FAMS", "FAM", "MARR",
                   "HUSB", "WIFE", "CHIL", "DIV", "DATE", "HEAD", "TRLR", "NOTE"]
-
-with open('CS555project2.ged') as file:
-    for line in file:
-        line = line.strip()
-        print("-->", line)
-        parts = line.split(" ")
-        level = parts[0]
-        tag = parts[1] if len(parts) > 1 else ""
-        valid = "Y" if tag in supported_tags else "N"
-        arguments = " ".join(parts[2:]) if len(parts) > 2 else ""
-        if arguments in supported_tags:
-            print("<--", level, "|", arguments, "|", "Y", "|", tag, sep="")
-        else:
-            print("<--", level, "|", tag, "|", valid, "|", arguments, sep="")
